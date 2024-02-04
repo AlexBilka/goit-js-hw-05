79daf7aea91723db69dcfc400fa1160d8cdd3283
AlexBilka <aleksandrbelka@gmail.com>
2024-02-04 02:22:30 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -7,7 +7,7 @@
 const getTotalBalanceByGender = (users, gender) =>
   users
     .filter(user => user.gender === gender)
-    .reduce((acc, user) => (acc += user.balance), 0);
+    .reduce((acc, user) => acc + user.balance, 0);
 
 const allUser = [
   {
